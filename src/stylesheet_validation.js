8b734bf2d95655c063b54c11c26d2128b4514599
siemiatj <kuba@saucelabs.com>
2017-03-20 21:34:16 +0100

    - some cleanup and renaming

--- src/stylesheet_validation.js
+++ src/stylesheet_validation.js
@@ -1,28 +1,27 @@
 import invariant from 'invariant';
 
 export default class NestedStyleSheetValidation {
-  static validateIsNestedStyle(nestedStyles) {
+  static validateNestedStyle(nestedStyles) {
     if (!__DEV__) {
         return;
     }
 
     // if you are nesting styles no parent element may have anything but
     // objects ({}) as children
-    for (var prop in nestedStyles) {
-      var styleObj = nestedStyles[prop];
+    for (let prop of Object.keys(nestedStyles)) {
+      const styleObj = nestedStyles[prop];
       if (Object.prototype.toString.call(styleObj) !== '[object Object]') {
-        styleError('"' + styleObj + '" is not a plain Javascript object', prop,
-          'StyleSheet ' + prop, 'Parents of nested styles can only have plain '
-          + 'Javascript objects ({...}) as children');
+        styleError(`"${styleObj}" is not a plain Javascript object`, prop,
+          `StyleSheet  ${prop}`, 'Parents of nested styles can only have plain \
+          Javascript objects ({...}) as children');
       }
     }
   }
 }
 
-const styleError = function(message1, style, caller?, message2?) {
+const styleError = function(message1, style, caller = '<<unknown>>', message2 = '') {
   invariant(
     false,
-    message1 + '\n' + (caller || '<<unknown>>') + ': ' +
-    JSON.stringify(style, null, '  ') + (message2 || '')
+    `${message1}\n${caller}: ${JSON.stringify(style, null, '  ')}${message2}`,
   );
 };
